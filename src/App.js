af444660c8498d15305855e70d5db71358056d80
Thom Miano <socratic.datum@gmail.com>
2018-10-20 17:28:34 -0400

    major overhaul filteredData

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import DatePicker from 'react-datepicker'
 import './App.css';
 import { history } from './data'
+
+const textFilter = (video, property, filterStr) => {
+  return filterStr && filterStr.length > 0 && video[property] ?
+    video[property].includes(filterStr) : true
+}
 
 const Video = (props) =>
   <div key={props.title}>
@@ -29,25 +32,20 @@ const VideoList = (props) =>
     }
   </div>
 
-class Filter extends Component {
-  render() {
-    return (
-      <div className="TextSearch">
-        <p>Title Search:</p>
-        <input type="text" onKeyUp={event =>
-          this.props.onTextChange(event.target.value)} />
-      </div>
-    )
-  }
-}
+const Filter = (props) =>
+  <div className="TextSearch">
+    <p>{props.label}:</p>
+    <input type="text" onKeyUp={event => props.onTextChange(event.target.value)} />
+  </div>
+
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      date: null,
       data: null,
       filterString: null,
+      dateString: null,
     }
   }
 
@@ -57,27 +55,28 @@ class App extends Component {
 
   render() {
     const data = this.state.data
+    const dateString = this.state.dateString
     const filterString = this.state.filterString
-    const filteredData =
-      filterString && filterString.length > 0 ?
-        data.filter(video => video.title.includes(this.state.filterString)) : data
-    const filteredCount = filteredData ? filteredData.length : -1
 
-    console.log(filteredCount)
+    const filteredData = data ?
+      data
+        .filter(video => textFilter(video, 'title', filterString))
+        .filter(video => textFilter(video, 'timestamp', dateString))
+      : []
+    const filteredCount = filteredData ? filteredData.length : -1
 
     return (
       <div className="App">
         <div className="Navigation">
-          {/* <DatePicker selected={this.state.date}/> */}
-          <Filter onTextChange={text => this.setState({ filterString: text })} />
+          <Filter label="Filter Date" onTextChange={text => this.setState({ dateString: text })} />
+          <Filter label="Filter Title" onTextChange={text => this.setState({ filterString: text })} />
+          <div className="FilteredCount">Number of results: {filteredCount}</div>
           {
             data ?
               <VideoList data={filteredData} /> :
               <div></div>
           }
-        </div>
-        <div>
-          {/* Put filteredCount here */}
+
         </div>
       </div>
     );
